b16ef6fc904864d4c0ba9b82982cd30eb525bef8
Sara Milan <sara@sharif.com>
2019-07-09 10:41:01 -0700

    WS: json parser

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
 app.post("/game", (req, res) => {
   // requires boardSize & mineCount
   // returns board (nested array of block statuses)
   // returns gameStatus = ongoing
-  res.send({ message: "you are asking for a new game" });
+  console.log(req.body.boardSize);
+  console.log(req.body.mineCount);
+  res.send({ message: `you are asking for a new game` });
 });
 
 app.post("/click", (req, res) => {
@@ -14,6 +18,8 @@ app.post("/click", (req, res) => {
   // requires click or flag (default to click)
   // returns grid
   // returns gameStatus = ongoing, lost, won
+  console.log(req.body.x);
+  console.log(req.body.y);
   res.send({ message: "you make a click!" });
 });
 
